a3aaf9302b73dcfe5c0c068f199dcd0f31b9d993
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-10-30 01:16:38 -0700

    Add tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
-var assert = require('assert');
+var assert = require('assert')
+
+describe('handleFormSubmission', () => {
+  it('should not send a POST request for severity High', function() {
+    assert.equal('High', handleFormSubmission(event))
+  })
+  it('should send a POST request for severity Low', function() {
+    assert.equal('Low', handleFormSubmission(event))
+  })
+  it('should send a POST request for severity Medium', function() {
+    assert.equal('Medium', handleFormSubmission(event))
+  })
+})
 
 describe('Array', function() {
   describe('#includes()', function() {
     it('should return 1 when the value is present', function() {
-      assert.equal(11, [1,2,3].includes(2));
-    });
-  });
-});
+      assert.equal(11, [1, 2, 3].includes(2))
+    })
+  })
+})
