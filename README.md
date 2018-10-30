a589d746d6c62abc7d4e2c513871f55b1f6b3a18
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-10-30 00:18:31 -0700

    part 1 done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # direct_form
+
+#### Part 1:
+
+- B. form attribute `required` was used for client side validation
+
+#### Part 2:
+
+#### Part 3:
---
+++ index.js
@@ -0,0 +1,21 @@
+function handleFormSubmission(event) {
+  event.preventDefault()
+
+  const form = document.querySelector('#contact_us_form')
+  const { name, email, question, severity } = form
+
+  const contactUsData = {
+    name: name.value.trim(),
+    email: email.value,
+    questionText: question.value,
+    id: '2c9985ab668037e00166b31c796134ef',
+    metadata: {
+      source_page: window.location.href,
+      date: new Date().toDateString(),
+      servity: severity.value,
+      simple_name: name.value.split(' ').length > 1 ? false : true,
+    },
+  }
+
+  DirectlyRTM('askQuestion', contactUsData)
+}
